e787258fd611b9b2428ac0d0e52d270f007de0b5
Sam <samuelmatthews96@gmail.com>
2024-06-30 15:31:05 +0100

    Initial commit - ScoresController

---
+++ Controllers/ScoresController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace DrvrApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScoresController : ControllerBase
+    {
+
+        [HttpGet("GetScores")]
+        public async Task<IActionResult> GetScores()
+        {
+            try
+            {
+                string filePath = "C:\\Users\\User\\source\\repos\\DrvrApi\\DrvrApi\\Controllers\\scores.txt";
+                string content = await System.IO.File.ReadAllTextAsync(filePath);
+                return Ok(content);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                return StatusCode(500, "Internal server error"); 
+            }
+        }
+    }
+}
